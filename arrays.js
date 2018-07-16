30aa862875ea567a42545bffdc22050f2461e6f6
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-07-16 21:07:31 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  return [element, ...array]
+  
+}
+
