8e44c4efd63af88f5d996a7f64145dee2599d6f7
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-07-16 22:15:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  var array1 = [ ...array, element];
+  var array = [ ...array, element];
   return array;
 }
 
