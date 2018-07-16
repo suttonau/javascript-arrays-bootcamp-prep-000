cdfbe381d48991d1da1d9ef17f52c399f2575e0a
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-07-16 21:40:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array, element) {
-  array.unshift(element);
-  return array;
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return [element, ...array]
+  array.unshift(element);
+  return array;
   
 }
 
